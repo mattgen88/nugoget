3b7d17ec09e18eff5a0faa8938f4433df1907019
Matthew General <matthew.general@gmail.com>
2021-04-01 17:16:00 -0400

    Add dependency locking ability

--- cmd/update.go
+++ cmd/update.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -19,11 +20,17 @@ var (
 	major  = false
 	patch  = false
 	dryRun = false
+	locks  []string
+)
+
+const (
+	ExitBadLock = iota
+	ExitBadCallOut
 )
 
 var (
 	projectRegexp         = regexp.MustCompile(`^(?:The given )?[Pp]roject ` + "`" + `([\w.]+)` + "`")
-	versionRegexp         = regexp.MustCompile(`^\s+\[([\w\d.]+)\]:`)
+	versionRegexp         = regexp.MustCompile(`^\s+\[([\w\d.]+)]:`)
 	headerRegexp          = regexp.MustCompile(`^\s+Top-level Package\s+Requested\s+Resolved\s+Latest`)
 	packageVersionsRegexp = regexp.MustCompile(`^\s+>\s+([\w.-]+)\s+([\d\w.-]+)\s+([\d\w.-]+)\s+([\d\w.-]+)`)
 	blankRegexp           = regexp.MustCompile(`$^`)
@@ -71,6 +78,20 @@ func newPackageData(name, requested, resolved, latest string) *packageData {
 }
 
 func update(cmd *cobra.Command, args []string) {
+	var lockedPackages = make(map[string]string)
+	if len(locks) > 0 {
+		// each lock should be in the pattern of <name>#<version>
+		for _, lock := range locks {
+			if !strings.Contains(lock, "#") {
+				fmt.Printf("Invalid lock %s should be in format <name>#<version>\n", lock)
+				os.Exit(ExitBadLock)
+			}
+			parts := strings.Split(lock, "#")
+			pkg := parts[0]
+			version := parts[1]
+			lockedPackages[pkg] = version
+		}
+	}
 	if major {
 		fmt.Println("Will do major upgrades!")
 	}
@@ -94,6 +115,7 @@ func update(cmd *cobra.Command, args []string) {
 	out, err := nugetCmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(ExitBadCallOut)
 		panic(err)
 	}
 
@@ -112,21 +134,22 @@ func update(cmd *cobra.Command, args []string) {
 			projects[p.name] = p
 			currentProject = p
 			fmt.Printf("Checking for updates in %s\n", p.name)
-			// fmt.Println("Project line")
 			break
 		case versionRegexp.MatchString(line):
-			// fmt.Println("Version line")
 			matches := versionRegexp.FindStringSubmatch(line)
 			currentProject.version = newVersion()
 			currentProject.version.name = matches[1]
 			break
 		case headerRegexp.MatchString(line):
-			// fmt.Println("Header line")
 			break
 		case packageVersionsRegexp.MatchString(line):
-			// fmt.Println("Package Versions String")
 			matches := packageVersionsRegexp.FindStringSubmatch(line)
-			currentProject.version.packageData[matches[1]] = newPackageData(matches[1], matches[2], matches[3], matches[4])
+			if _, ok := lockedPackages[matches[1]]; ok {
+				currentProject.version.packageData[matches[1]] = newPackageData(matches[1], lockedPackages[matches[1]], lockedPackages[matches[1]], lockedPackages[matches[1]])
+				updates++
+			} else {
+				currentProject.version.packageData[matches[1]] = newPackageData(matches[1], matches[2], matches[3], matches[4])
+			}
 			if matches[2] != matches[4] {
 				updates++
 			}
@@ -151,7 +174,7 @@ func update(cmd *cobra.Command, args []string) {
 				nugetCmd = exec.Command(path, "add", project.name, "package", dependency.name, "-v", dependency.latest)
 				out, err = nugetCmd.CombinedOutput()
 				if err != nil {
-					panic(err)
+					os.Exit(ExitBadCallOut)
 				}
 
 				fmt.Println(string(out))
@@ -165,4 +188,5 @@ func init() {
 	updateCmd.PersistentFlags().BoolVarP(&major, "major", "M", false, "When true, will update to latest major. This can break your project!")
 	updateCmd.PersistentFlags().BoolVarP(&patch, "patch", "P", false, "When true, will update to latest patch.")
 	updateCmd.PersistentFlags().BoolVarP(&dryRun, "dryrun", "D", false, "When true, will only do a dry run, no changes will be made.")
+	updateCmd.PersistentFlags().StringArrayVarP(&locks, "lock", "l", []string{}, "list of locked packages and their versions in the format of <packagename>#version. All instances of each package will be set to that version specifically")
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/mattgen88/nugoget
 go 1.15
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/spf13/cobra v1.1.3
 )
